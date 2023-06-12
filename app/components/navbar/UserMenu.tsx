6a8ac764c334c6f416006b5a89ad2ddaf29f2d64
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-06-12 11:48:13 +0300

    Making trips screen

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -8,12 +8,14 @@ import MenuItem from './MenuItem'
 import { signOut } from 'next-auth/react'
 import { SafeUser } from '@/app/types'
 import useRentModal from '@/app/hooks/useRentModal'
+import { useRouter } from 'next/navigation'
 
 interface UserMenuProps {
     currentUser: SafeUser | null
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const router = useRouter()
   const registerModal = useRegisterModal()
   const loginModal = useLoginModal()
   const rentModal = useRentModal()
@@ -49,7 +51,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 <div className='flex flex-col cursor-pointer'>
                     {currentUser ? (
                         <>
-                            <MenuItem onClick={() => {}} label="My trips" />
+                            <MenuItem onClick={() => router.push('/trips')} label="My trips" />
                             <MenuItem onClick={() => {}} label="My favorites" />
                             <MenuItem onClick={() => {}} label="My reservation" />
                             <MenuItem onClick={() => {}} label="My propertiese" />
