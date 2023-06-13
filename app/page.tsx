831bfb227fdeaf0616b4f21d94cd2ae4e2b55002
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-06-13 14:34:36 +0300

    Debug searchParams error

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
-import getCurrentUser from "./actions/getCurrentUser";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listings/ListingCard";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
 
 import getListings, { 
   IListingsParams
 } from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 interface HomeProps {
   searchParams: IListingsParams
 };
 
-const Home = async ({searchParams} : HomeProps) => {
-  const listings = await getListings(searchParams)
-  const currentUser = await getCurrentUser()
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
-      <EmptyState showReset />
-    )
+        <EmptyState showReset />
+    );
   }
 
   return (
-    <Container>
+      <Container>
         <div className="pt-24 grid  grid-cols-1  sm:grid-cols-2  md:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {listings.map((listing: any) => (
             <ListingCard
@@ -32,8 +32,8 @@ const Home = async ({searchParams} : HomeProps) => {
             />
           ))}
         </div>
-    </Container>
+      </Container>
   )
 }
 
-export default Home
+export default Home;
